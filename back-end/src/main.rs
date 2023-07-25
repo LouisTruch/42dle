11e8ecb9346d6f9b4682f4868542a7e6275e285c
Armandase <adamiens@student.42angouleme.fr>
2023-07-25 14:37:54 +0200

    connection wiht 42's api in progress

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -5,12 +7,22 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/ohoh")]
-fn a() -> &'static str {
-    "baby calme down!"
+#[get("/token/<code>")]
+async fn token(code: &str) -> String {
+    // format!("Hello, new user: {}", name)
+    let client = reqwest::Client::new();
+    let res = client.post("https://api.intra.42.fr/oauth/token")
+    .header("grant_type", "authorization_code")
+    .header("client_id", env::var("CLIENT_ID").unwrap())
+    .header("client_secret", env::var("CLIENT_SECRET").unwrap())
+    .header("code", code)
+    // .header("redirect_uri", uuid)
+    .send()
+    .await?;
+    "Hello, your code is ".to_string() + code
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, a])
+    rocket::build().mount("/", routes![index, token])
 }

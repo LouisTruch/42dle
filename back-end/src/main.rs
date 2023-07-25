7ba383ee2085ed21559a4509df64ff1f57fafc8b
Skylow667 <nathan.locussol@gmail.com>
2023-07-25 14:36:34 +0200

    .env and connection to DB

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,3 +1,7 @@
+use mysql::prelude::*;
+use mysql::Pool;
+use std::env;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -12,5 +16,17 @@ fn a() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
+    let link: String = format!("mysql://{}:{}@mysql/{}", env::var("MYSQL_USER").unwrap(), env::var("MYSQL_PASSWORD").unwrap(), env::var("DB_NAME").unwrap());
+    let pool = Pool::new(link.as_str()).unwrap();
+    let mut conn = pool.get_conn().unwrap();
+
+    conn.query_drop(
+        r"
+        CREATE TABLE IF NOT EXISTS users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            login TEXT NOT NULL
+        )",
+    ).unwrap();
+
     rocket::build().mount("/", routes![index, a])
 }

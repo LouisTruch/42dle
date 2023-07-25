5629c08d3e244fa9b864518282e1124050ee9636
Nathan Locussol <nlocusso@1C2.42angouleme.fr>
2023-07-25 16:38:53 +0200

    for merge

--- back-end/src/main.rs
+++ back-end/src/main.rs
@@ -1,8 +1,7 @@
-use mysql::prelude::*;
-use mysql::Pool;
 use std::env;
-
-#[macro_use] extern crate rocket;
+use sqlx::{mysql::MySqlPool, Pool, MySqlConnection, Row};
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -16,17 +15,5 @@ fn a() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    let link: String = format!("mysql://{}:{}@mysql/{}", env::var("MYSQL_USER").unwrap(), env::var("MYSQL_PASSWORD").unwrap(), env::var("DB_NAME").unwrap());
-    let pool = Pool::new(link.as_str()).unwrap();
-    let mut conn = pool.get_conn().unwrap();
-
-    conn.query_drop(
-        r"
-        CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            login TEXT NOT NULL
-        )",
-    ).unwrap();
-
-    rocket::build().mount("/", routes![index, a])
+    rocket::build().mount("/", routes![index, a]);
 }

051c4af51507740be6e4f289a6ee89e6b94c12b4
Nathan Locussol <nlocusso@1B2.42angouleme.fr>
2023-07-27 19:54:59 +0200

    comment the code in back-end db

--- back-end/src/users.rs
+++ back-end/src/users.rs
@@ -8,6 +8,7 @@ pub async fn new_user(
     login: &String, profile_pic: &String
 ) -> Result<InsertResult<users::ActiveModel>, DbErr> {
 
+    // Create a record to add in db
     let record = users::ActiveModel {
         login: Set(login.to_owned()),
         profile_pic: Set(profile_pic.to_owned()),
@@ -15,32 +16,27 @@ pub async fn new_user(
         ..Default::default()
     };
 
+    // Insert in db and return the Result
     Users::insert(record).exec(db).await
-    //     Ok(_) => println!("User Created!"),
-    //     Err(e) => { 
-    //         println!("{}", e);
-    //         return status::BadRequest(Some("User already in database!"));
-    //     }
-    // };
-    // // .save(db)
-    // // .await
-    // status::Accepted(Some("New User Created!"))
 }
 
 pub async fn update_user_by_login(
     db: &DatabaseConnection,
     data: users::Model,
 ) -> Result<users::Model, DbErr> {
+
+    // Find users in db with login and update with new score
     let users: users::ActiveModel = Users::find_by_id(data.login)
         .one(db)
         .await?
         .ok_or(DbErr::Custom("Cannot find post.".to_owned()))
         .map(Into::into)?;
 
+    // user to update 
     users::ActiveModel {
         login: users.login,
         score: Set(data.score.to_owned()),
-        profile_pic: Set(data.profile_pic.to_owned()),
+        profile_pic: users.profile_pic,
     }
     .update(db)
     .await

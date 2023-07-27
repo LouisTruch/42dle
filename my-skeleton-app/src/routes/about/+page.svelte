cfb0f94ce9d6d347a5e1f5b87729c1bb78899070
Louis Truchelut <louis.truchelut@gmail.com>
2023-07-27 12:32:38 +0200

    added Skeleton UI to the project, need to launch in skeleton-app and not front-end

---
+++ my-skeleton-app/src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<br />
+<div class="container h-full mx-auto justify-center items-center">
+	<h1 class="text-l uppercase">About us</h1>
+	Made by<a class="anchor" href="https://profile.intra.42.fr/users/adamiens"> Adamiens</a>,
+	<a class="anchor" href="https://profile.intra.42.fr/users/nlocusso">Nlocusso</a> and
+	<a class="anchor" href="https://profile.intra.42.fr/users/ltruchel">Ltruchel</a>.
+	<br />
+	<br />
+	Made with
+	<a href="https://kit.svelte.dev/" class="anchor">SvelteKit (Front-End)</a>,
+	<a href="https://rocket.rs/" class="anchor">Rocket (Back-End)</a> and
+	<a href="https://www.postgresql.org/" class="anchor">PostgreSQL</a>.
+</div>

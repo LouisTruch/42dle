a18a5bb83b2da01d6a48127df3fd9393e0e729d8
Louis Truchelut <louis.truchelut@gmail.com>
2023-08-09 16:53:36 +0200

    Admin dashboard begining

---
+++ my-skeleton-app/src/routes/api/admin/fill-db/+server.ts
@@ -0,0 +1,22 @@
+import type { RequestEvent } from '@sveltejs/kit';
+
+export async function GET(requestEvent: RequestEvent) {
+	const { fetch, request } = requestEvent;
+
+	console.log(request);
+	let cookies = request.headers.get('cookie');
+	if (cookies == null) {
+		cookies = '';
+	} else {
+		const pos = cookies.search('user_id');
+		if (pos < 0) {
+			cookies = '';
+		}
+	}
+	const response = await fetch('http://127.0.0.1:8000/game/update-db', {
+		method: 'GET',
+		credentials: 'include',
+		headers: { cookie: cookies },
+	});
+	return response;
+}
